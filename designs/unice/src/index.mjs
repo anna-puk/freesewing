fff210c284f3e95dfc59cb0c8f397ea240508e94
anna-puk <100537439+anna-puk@users.noreply.github.com>
2022-09-10 18:44:17 +0200

    attempt to restore markers
    formerly inherited from ursula, now part of unice itself

--- designs/unice/src/index.mjs
+++ designs/unice/src/index.mjs
@@ -5,36 +5,30 @@ import config from '../config'
 import draftFront from './front'
 import draftBack from './back'
 import draftGusset from './gusset'
+import draftElastic from './elastic'
+import draftFront2 from './front2'
+import draftBack2 from './back2'
+import draftGusset2 from './gusset2'
 
 // Create new design
 const Unice = new freesewing.Design(config, plugins)
 
-// Attache Ursula parts that we will extend
+// Attach Ursula parts that we will extend
 for (const name of ['Back', 'Front', 'Gusset']) {
   Unice.prototype[`draftUrsula${name}`] = function (part) {
     return new Ursula(this.settings)[`draft${name}`](part)
   }
 }
 
-// Attach Ursula part that we'll use as-is
-Unice.prototype.draftElastic = function (part) {
-  return new Ursula(this.settings).draftElastic(part)
-}
-Unice.prototype.draftFront2 = function (part) {
-  return new Ursula(this.settings).draftFront2(part)
-}
-Unice.prototype.draftBack2 = function (part) {
-  return new Ursula(this.settings).draftBack2(part)
-}
-Unice.prototype.draftGusset2 = function (part) {
-  return new Ursula(this.settings).draftGusset2(part)
-}
-
-
 // Attach our own draft methods to the prototype
 Unice.prototype.draftFront = draftFront
 Unice.prototype.draftBack = draftBack
 Unice.prototype.draftGusset = draftGusset
+Unice.prototype.draftElastic = draftElastic
+Unice.prototype.draftFront2 = draftFront2
+Unice.prototype.draftBack2 = draftBack2
+Unice.prototype.draftGusset2 = draftGusset2
+
 
 // Named exports
 export { config, Unice }
