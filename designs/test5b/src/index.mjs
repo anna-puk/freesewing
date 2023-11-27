1d0e418c7a1b89325778210f6a30119d476978a6
anna-puk <anna_puk@tux-orbital.org>
2023-11-27 21:35:55 +0000

    move lily contents to test5b for further testing

--- designs/test5b/src/index.mjs
+++ designs/test5b/src/index.mjs
@@ -5,12 +5,15 @@ import { i18n } from '../i18n/index.mjs'
 import { data } from '../data.mjs'
 // Parts
 import { box } from './box.mjs'
+import { back } from './back.mjs'
+import { front } from './front.mjs'
+import { waistband } from './waistband.mjs'
 
 // Create new design
 const Test5b = new Design({
   data,
-  parts: [box],
+  parts: [box, back, front, waistband],
 })
 
 // Named exports
-export { box, i18n, Test5b }
+export { box, back, front, waistband, i18n, Test5b }
